dc0c72f0c921b754f739670b00eb90832e2468d0
kanisaur <seto3ka4u@outlook.jp>
2024-11-18 13:16:46 +0900

    書き換えました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -23,26 +23,67 @@ void initialize_list(list* l)
 // 先頭にデータを追加
 void push_front(list* l, node* p)
 {
+	if (l->header == NULL)
+	{
+		l->header = p;
+		l->footer = p;
+	}
+	else {
+		p->pNext = l->header;
+		l->header->pPrev = p;
+		l->header = p;
+	}
 }
 
 // 末尾にデータを追加
 void push_back(list* l, node* p)
 {
+	if (l->footer == NULL)
+	{
+		l->header = p;
+		l->footer = p;
+	}
+	else {
+		p->pPrev = l->footer;
+		l ->footer->pNext = p;
+		l->footer = p;
+	}
 }
 
 // pのノードを削除
 void remove_node(list* l, node* p)
 {
+	if (p == NULL)return;
+	if (p->pPrev != NULL)
+	{
+		p->pPrev->pNext = p->pNext;
+	}
+	else {
+		l->header = p->pNext;
+	}
+	if (p->pNext != NULL)
+	{
+		p->pNext->pPrev = p->pPrev;
+	}
+	else {
+		l->footer = p->pPrev;
+	}
+	p->pNext = NULL;
+	p->pPrev = NULL;
 }
 
 // pの次のノードを削除
 void remove_next(list* l, node* p)
 {
+	if (p == NULL || p->pNext == NULL)return;
+	remove_node(l, p->pNext);
 }
 
 // pの前のノードを削除
 void remove_prev(list* l, node* p)
 {
+	if (p == NULL || p->pPrev == NULL)return;
+	remove_node(l, p->pPrev);
 }
 
 
